19c37bc3478366769cf9b39b4f8906d073218a04
chandanjha333 <162859024+chandanjha333@users.noreply.github.com>
2026-07-27 14:43:24 +0530

    redis container integration

--- farm-marketplace/backend/index.js
+++ farm-marketplace/backend/index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth');
 const connectDB = require('./config/db');
 const User = require('./models/User');
-const ratelimiter = require('./middleware/ratelimiter');
+const { rateLimiter } = require('./middleware/ratelimiter');
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -45,34 +45,34 @@ const verifyToken = (req, res, next) => {
 // Routes
 app.use('/api/auth', authRoutes);
 app.use(express.static(path.join(__dirname, '../frontend')));
-app.use(ratelimiter(100, 60));
+app.use(rateLimiter(100, 60));
 
-app.get('/', ratelimiter(30, 60), (req, res) => {
+app.get('/', rateLimiter(30, 60), (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'homepage.html'));
 });
 
-app.get('/login', ratelimiter(30, 60), (req, res) => {
+app.get('/login', rateLimiter(30, 60), (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'login.html'));
 });
 
-app.get('/signup', ratelimiter(30, 60), (req, res) => {
+app.get('/signup', rateLimiter(30, 60), (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'signup.html'));
 });
 
-app.get('/products', ratelimiter(30, 60), (req, res) => {
+app.get('/products', rateLimiter(30, 60), (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'products.html'));
 });
 
-app.get('/contacts', ratelimiter(30, 60), (req, res) => {
+app.get('/contacts', rateLimiter(30, 60), (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'contactus.html'));
 });
 
-app.get('/api/me', ratelimiter(5, 60), verifyToken, async (req, res) => {
+app.get('/api/me', rateLimiter(5, 60), verifyToken, async (req, res) => {
   const user = await User.findById(req.user._id);
   res.json({ name: user.name, email: user.email });
 });
 
-app.post('/api/logout', ratelimiter(5, 60), (req, res) => {
+app.post('/api/logout', rateLimiter(5, 60), (req, res) => {
   res.clearCookie('token');
   res.json({ success: true });
 });
